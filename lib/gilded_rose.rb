3227b0d80caae40ce7eb15c9979177acbe298c72
Alan Ren <alanlin786@gmail.com>
2024-03-13 00:14:43 -0500

    final solution, remove the x and add extract configuration data

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,21 +1,4 @@
 module GildedRose
-  def self.new(name:, days_remaining:, quality:)
-    klass_for(name).new(quality, days_remaining)
-  end
-
-  def self.klass_for(name)
-    case name
-    when 'Normal Item'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
-  end
-
   class Item
     attr_reader :quality, :days_remaining
     def initialize(quality, days_remaining)
@@ -45,12 +28,6 @@ module GildedRose
     end
   end
 
-  class Sulfuras < Item
-    def tick
-
-    end
-  end
-
   class Backstage < Item
     def tick
       @days_remaining -= 1
@@ -62,4 +39,25 @@ module GildedRose
       @quality += 1 if @days_remaining < 5
     end
   end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASS = {
+    'Normal Item'                               => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    'Conjured Mana Cake'                        => Conjured }
+  def self.new(name:, days_remaining:, quality:)
+    (SPECIALIZED_CLASS[name] || DEFAULT_CLASS).
+      new(quality, days_remaining)
+  end
+  
 end
